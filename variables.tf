08371426e30b1f65c033aedaccb75c4166d2e2c5
Igor Rubio <ighor_souza@hotmail.com>
2024-06-24 18:28:57 -0300

    cleaned files

--- variables.tf
+++ variables.tf
@@ -9,18 +9,3 @@ variable "aws_profile" {
   type    = string
 }
 
-#infra_subnet1_id = module.infrastructure.aws_subnet.subnet-publica1.id
-
-
-#subnet_id = module.infrastructure.subnet_publica1_id
-
-/*
-variable "infra_vpc_id" {
-  description = aws_vpc.id_vpc
-}
-variable "infra_subnet2_id" {
-  description = aws_subnet.infra_subnet2_id
-}
-
-
-*/
